bbc8f8440b8fabc9d9c340fbdea0dc576639400c
banteg <banteeg@gmail.com>
2020-10-26 18:43:01 +0300

    feat: add vault and strategy fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 
 with open("configurations.json") as f:
-    configuration = json.load(f)
+    configurations = json.load(f)
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -11,108 +11,52 @@ def shared_setup(fn_isolation):
     pass
 
 
-@pytest.fixture(scope="module", params=configuration["vaults"])
+@pytest.fixture(scope="module", params=configurations["vaults"][:1])
 def config(request):
-    return request.param
+    return {**configurations["common"], **request.param}
 
 
 @pytest.fixture
-def andre(accounts):
-    # Andre, giver of tokens, and maker of yield
-    yield accounts[0]
+def vault(config, Vault, gov, rewards, guardian):
+    return Vault.deploy(
+        config["want"],
+        gov,
+        rewards,
+        config["name"],
+        config["symbol"],
+        {"from": guardian},
+    )
 
 
 @pytest.fixture
-def token(andre, Token):
-    yield andre.deploy(Token)
+def strategy(config, StrategyUniswapPairPickle, vault, strategist, keeper):
+    strategy = StrategyUniswapPairPickle.deploy(
+        vault, config["jar"], config["pid"], {"from": strategist}
+    )
+    strategy.setKeeper(keeper, {"from": strategist})
+    return strategy
 
 
 @pytest.fixture
 def gov(accounts):
-    # yearn multis... I mean YFI governance. I swear!
-    yield accounts[1]
+    return accounts[1]
 
 
 @pytest.fixture
 def rewards(gov):
-    yield gov  # TODO: Add rewards contract
+    return gov
 
 
 @pytest.fixture
 def guardian(accounts):
-    # YFI Whale, probably
-    yield accounts[2]
+    return accounts[2]
 
 
 @pytest.fixture
 def strategist(accounts):
-    # You! Our new Strategist!
-    yield accounts[3]
+    return accounts[3]
 
 
 @pytest.fixture
 def keeper(accounts):
-    # This is our trusty bot!
-    yield accounts[4]
-
-
-@pytest.fixture
-def nocoiner(accounts):
-    # Has no tokens (DeFi is a ponzi scheme!)
-    yield accounts[5]
-
-
-@pytest.fixture
-def pleb(accounts, andre, token, vault):
-    # Small fish in a big pond
-    a = accounts[6]
-    # Has 0.01% of tokens (heard about this new DeFi thing!)
-    bal = token.totalSupply() // 10000
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
-
-@pytest.fixture
-def chad(accounts, andre, token, vault):
-    # Just here to have fun!
-    a = accounts[7]
-    # Has 0.1% of tokens (somehow makes money trying every new thing)
-    bal = token.totalSupply() // 1000
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
-
-@pytest.fixture
-def greyhat(accounts, andre, token, vault):
-    # Chaotic evil, will eat you alive
-    a = accounts[8]
-    # Has 1% of tokens (earned them the *hard way*)
-    bal = token.totalSupply() // 100
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
-
-@pytest.fixture
-def whale(accounts, andre, token, vault):
-    # Totally in it for the tech
-    a = accounts[9]
-    # Has 10% of tokens (was in the ICO)
-    bal = token.totalSupply() // 10
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
+    return accounts[4]
