448a67a3a1c1f3da64d3a006465f5234d1bd814e
banteg <banteeg@gmail.com>
2020-10-26 19:08:58 +0300

    feat: source want lp tokens

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ def shared_setup(fn_isolation):
     pass
 
 
-@pytest.fixture(scope="module", params=configurations["vaults"][:1])
+@pytest.fixture(scope="module", params=configurations["vaults"])
 def config(request):
     return {**configurations["common"], **request.param}
 
@@ -38,6 +38,42 @@ def strategy(config, StrategyUniswapPairPickle, vault, strategist, keeper):
 
 
 @pytest.fixture
+def token(config, whale, uniswap, interface, weth, chain):
+    weth.approve(uniswap, 2 ** 256 - 1, {"from": whale})
+    pair = interface.UniswapPair(config["want"])
+    tokens = [interface.ERC20(token) for token in [pair.token0(), pair.token1()]]
+    amount_in = "5000 ether"
+    # obtain 10000 eth worth of liquidity
+    for token in tokens:
+        if token.allowance(whale, uniswap) == 0:
+            token.approve(uniswap, 2 ** 256 - 1, {"from": whale})
+
+        if token != weth:
+            uniswap.swapExactTokensForTokens(
+                amount_in,
+                0,
+                [weth, token],
+                whale,
+                chain[-1].timestamp + 1200,
+                {"from": whale},
+            )
+    # obtain want token by adding liquidity
+    uniswap.addLiquidity(
+        tokens[0],
+        tokens[1],
+        tokens[0].balanceOf(whale) if tokens[0] != weth else amount_in,
+        tokens[1].balanceOf(whale) if tokens[1] != weth else amount_in,
+        0,
+        0,
+        whale,
+        chain[-1].timestamp + 1200,
+        {"from": whale},
+    )
+    print(pair.balanceOf(whale).to("ether"), "want obtained")
+    return pair
+
+
+@pytest.fixture
 def gov(accounts):
     return accounts[1]
 
@@ -60,3 +96,19 @@ def strategist(accounts):
 @pytest.fixture
 def keeper(accounts):
     return accounts[4]
+
+
+@pytest.fixture
+def whale(accounts):
+    # makerdao eth-a join adapter, this address has a ton of weth
+    return accounts.at("0x2F0b23f53734252Bda2277357e97e1517d6B042A", force=True)
+
+
+@pytest.fixture
+def weth(interface):
+    return interface.ERC20("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
+
+
+@pytest.fixture
+def uniswap(interface, weth, whale):
+    return interface.UniswapRouter("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
